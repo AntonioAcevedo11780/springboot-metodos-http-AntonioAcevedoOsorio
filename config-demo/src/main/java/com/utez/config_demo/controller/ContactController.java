f821f13da51d555e1621a1263a9599376a9b7755
Antonio Acevedo Osorio <20243ds076@utez.edu.mx>
2025-11-19 23:59:26 -0600

    si
    si

---
+++ config-demo/src/main/java/com/utez/config_demo/controller/ContactController.java
@@ -0,0 +1,24 @@
+package com.utez.config_demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.utez.config_demo.dto.ContactRequestDto;
+
+@RestController
+@RequestMapping("/api")
+public class ContactController {
+
+    @GetMapping("/info")
+    public ResponseEntity<String> info() {
+        return ResponseEntity.ok("API funcionando correctamente.");
+    }
+
+    @PostMapping("/contact")
+    public ResponseEntity<String> contact(@RequestBody ContactRequestDto body) {
+        if (body.getName() == null || body.getMessage() == null) {
+            return ResponseEntity.badRequest().body("Faltan datos obligatorios.");
+        }
+        return ResponseEntity.status(201)
+                .body("Mensaje recibido de " + body.getName() + ": " + body.getMessage());
+    }
+}
